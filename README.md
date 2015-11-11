37adffdcd95997cfbc7d874f9fa34e838c01c97b
Erwin Yusrizal <erwin.yusrizal@gmail.com>
2015-11-11 08:38:28 +0800

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# regexrangenumbergenerator
-Simple Regex Range Number Generator
+# Regex Numeric Range Generator
+Simple Regex Numeric Range Generator - inspired by <http://utilitymill.com/utility/Regex_For_Range> since the website is down very often, I create this version using Javascript
+
+## Usage
+
+### Options
+```
+MatchWholeWord: Boolean (default: false)
+MatchWholeLine: Boolean (default: false)
+MatchLeadingZero: Boolean (default: false)
+showProcess: Boolean (default: false)
+```
+
+### Save The Output in a variable
+```
+var strReg = RegNumericRange(minValue, maxValue, {
+    MatchWholeWord: mww ? true : false,
+    MatchWholeLine: mwl ? true : false,
+    MatchLeadingZero: mlz ? true : false,
+    showProcess: verbose ? true : false
+}).generate();
+```
+
+### Use Callback
+```
+RegNumericRange(minValue, maxValue, {
+    MatchWholeWord: mww ? true : false,
+    MatchWholeLine: mwl ? true : false,
+    MatchLeadingZero: mlz ? true : false,
+    showProcess: verbose ? true : false
+}).generate(function(result){
+    $('#loader').hide();
+    if(result.success){
+        $('#message').empty();
+        $('#result textarea').val(result.pattern);
+    }else{
+        $('#message').html('<div class="error">'+result.message+'</div>');
+    }
+});
+```
+
+## Todos
+ - Make this support float number
---
+++ app.js
@@ -0,0 +1,42 @@
+$(function(){
+    $('input').on("keydown", function(e){
+
+        if (e.keyCode == 46 || e.keyCode == 8 || e.keyCode == 9 || e.keyCode == 27 || e.keyCode == 13 || (e.keyCode == 65 && e.ctrlKey === true) || (e.keyCode == 67 && e.ctrlKey === true) || (e.keyCode == 86 && e.ctrlKey === true) || (e.keyCode == 88 && e.ctrlKey === true) || (e.keyCode >= 35 && e.keyCode <= 39)) {
+            return;
+        } else {
+            if (e.shiftKey || (e.keyCode < 48 || e.keyCode > 57) && (e.keyCode < 96 || e.keyCode > 105)) {
+                e.preventDefault();
+            }
+        }
+    });
+
+    $('form#generate').on("submit", function(e){
+        e.preventDefault();
+
+        var minValue = $('input#min-value').val().trim(),
+            maxValue = $('input#max-value').val().trim(),
+            mww = $('input[name="mww"]:checked').val(),
+            mwl = $('input[name="mwl"]:checked').val(),
+            mlz = $('input[name="mlz"]:checked').val(),
+            verbose = $('input[name="verbose"]:checked').val();
+
+        RegNumericRange(minValue, maxValue, {
+          MatchWholeWord: mww ? true : false,
+          MatchWholeLine: mwl ? true : false,
+          MatchLeadingZero: mlz ? true : false,
+          showProcess: verbose ? true : false
+        }).generate(function(result){
+            $('#loader').hide();
+            if(result.success){
+                $('#message').empty();
+                $('#result textarea').val(result.pattern);
+            }else{
+                $('#message').html('<div class="error">'+result.message+'</div>');
+            }
+        });
+
+    });
+
+    var clipboard = new Clipboard('#copytoclipboard');
+
+});
